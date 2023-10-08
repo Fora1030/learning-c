9d2bfb82c78f6db531d132428227066d12c3a0ae
Franklin Rodriguez <franklin.r.git@gmail.com>
2023-10-08 07:37:26 -0400

    summary: practice the use of for loop statemets in C

--- loopsAndIterations/loops.c
+++ loopsAndIterations/loops.c
@@ -4,10 +4,13 @@ int sumNviaGauss(int N);
 
 int sumWhileLoop(int N);
 
+int sumForLoop(int N);
+
 int main(void) 
 {   
     printf("Rresult of adding 100 = %d using Gauss method\n", sumNviaGauss(100));
     printf("Rresult of adding 100 = %d using while loop\n", sumWhileLoop(100));
+    printf("Rresult of adding 100 = %d using for loop\n", sumForLoop(100));
     return 0;
 }
 
@@ -30,5 +33,17 @@ int sumWhileLoop(int N)
 
         num++;
     }
+    return sum;
+}
+
+int sumForLoop(int N)
+{
+    int sum = 0;
+
+    for(int num = 0; num < N; num++)
+    {
+        sum += (num + 1);
+    }
+
     return sum;
 }
