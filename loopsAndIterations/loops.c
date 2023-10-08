1ac2b5c3dd2967fe27b1ec57421fbb40c20ec714
Franklin Rodriguez <franklin.r.git@gmail.com>
2023-10-08 08:08:31 -0400

    using doto statements

--- loopsAndIterations/loops.c
+++ loopsAndIterations/loops.c
@@ -8,12 +8,15 @@ int sumForLoop(int N);
 
 int sumDoWhile(int N);
 
+int sumGoto(int N);
+
 int main(void) 
 {   
     printf("Rresult of adding 100 = %d using Gauss method\n", sumNviaGauss(100));
     printf("Rresult of adding 100 = %d using while loop\n", sumWhileLoop(100));
     printf("Rresult of adding 100 = %d using for loop\n", sumForLoop(100));
     printf("Rresult of adding 100 = %d using do While\n", sumDoWhile(100));
+    printf("Rresult of adding 100 = %d using goto statements\n", sumGoto(100));
     return 0;
 }
 
@@ -63,4 +66,18 @@ int sumDoWhile(int N)
     } while (num < N);
 
     return sum;
+}
+
+int sumGoto(int N)
+{
+    int sum = 0;
+
+    int num = 0;
+
+    start_loop:
+        sum += (num + 1);
+        num++;
+        if ( num < N) goto start_loop;
+    end_loop:
+        return sum;
 }
