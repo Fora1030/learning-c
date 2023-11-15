a9e559710198bd05c817d920f1827362893ba40c
Franklin Rodriguez <franklin.r.git@gmail.com>
2023-11-15 08:00:05 -0500

    working with formated input

--- io/reading_string.c
+++ io/reading_string.c
@@ -4,8 +4,13 @@ enum { kBufferSize = 80 };
 
 int main( void )  {
     char stringBuffer[ kBufferSize ] = {0};
+    char stringBuffer2[ kBufferSize ] = {0};
     printf( "Enter a string: " );
     scanf(  "%s" , stringBuffer );
     printf( "Processed string: [%s]\n", stringBuffer );
+
+    printf("Enter another string press enter (/) to complete string:");
+    scanf("%[^/]", stringBuffer2);
+    printf("String: [%s]\n", stringBuffer2);
     return 0;
 }
